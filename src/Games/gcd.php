a5dfce2a598ab8bfe74c27e4d682d51eb40af371
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-08-06 19:34:05 +0700

    Add files that I forgot to add to the previous commit

---
+++ src/Games/gcd.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BrainGames\Games;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\greeting;
+use function BrainGames\Engine\task;
+use function BrainGames\Engine\gameProcess;
+
+function gcdCalculating($num1, $num2)
+{
+    $smallerValue = min($num1, $num2);
+    while (true) {
+        if ((($num1 % $smallerValue) === 0) and (($num2 % $smallerValue) === 0)) {
+            return $smallerValue;
+        }
+        $smallerValue--;
+    }
+}
+
+function gcdIteration()
+{
+    $randNumber1 = rand(0, 100);
+    $randNumber2 = rand(0, 100);
+    $rightAnswer = gcdCalculating($randNumber1, $randNumber2);
+    line("Question: %d %d", $randNumber1, $randNumber2);
+    $answer = prompt("Your answer");
+    if ($answer == $rightAnswer) {
+        return "Right!";
+    } else {
+        line("'%d' is wrong answer ;(. Correct answer was '%d'.", $answer, $rightAnswer);
+        return "Mistake";
+    }
+}
+
+function gcd()
+{
+    greeting();
+    task('Find the greatest common divisor of given numbers.');
+    gameProcess('gcd');
+}
