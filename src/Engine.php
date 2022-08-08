d9bee033f51a5af81d927c2cfb41be83f375afa0
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-08-08 18:18:48 +0700

    Debug step 9

--- src/Engine.php
+++ src/Engine.php
@@ -21,6 +21,7 @@ function task(string $text)
 function gameProcess(string $gameName)
 {
     global $name;
+    $result = '';
     for ($rightAnswerCount = 1; $rightAnswerCount < 4; $rightAnswerCount++) {
         if (is_callable('BrainGames\Games\\' . $gameName)) {
             $result = call_user_func('BrainGames\Games\\' . $gameName);
